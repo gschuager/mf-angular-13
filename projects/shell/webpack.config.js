a6eea5a676487c71be5b84e96d96cbc4cdf84b3a
Germán Schuager <gschuager@gmail.com>
2021-12-09 10:03:11 -0300

    Fixed dep issue + simplified custom webpack config

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,12 +1,4 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
-
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, '../../tsconfig.json'),
-  [/* mapped paths to share */]);
 
 module.exports = {
   output: {
@@ -16,38 +8,18 @@ module.exports = {
   },
   optimization: {
     runtimeChunk: false,
-    // minimize: false
+    //minimize: false
   },
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-
   plugins: [
     new ModuleFederationPlugin({
-
-      name: "shell",
-
-        // For hosts (please adjust)
-        remotes: {
-          // Load as module
-          // "mfe1": "http://localhost:3000/remoteEntry.js",
-        },
-
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          '@ngrx/effects': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          '@ngrx/entity': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-          '@ngrx/store': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-
-          ...sharedMappings.getDescriptors()
-        })
-
+      shared: {
+        '@angular/core': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' },
+        '@angular/common': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' },
+        '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' },
+        '@angular/router': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' },
+        '@ngrx/effects': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' },
+        '@ngrx/store': { singleton: true, strictVersion: true, requiredVersion: '13.0.2' }
+      }
     }),
-    sharedMappings.getPlugin()
   ],
 };
